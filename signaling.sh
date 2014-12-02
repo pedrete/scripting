43069353ada376c5dff96fee30140407492836e0
PEDRO J GOMEZ OCHOA <pedrete@gmail.com>
2014-12-02 17:08:23 +0100

    testing

--- signaling.sh
+++ signaling.sh
@@ -6,3 +6,17 @@ echo "pid es $$"
 while true; do sleep 1; done
 
 echo fin
+
+#:(){
+#echo hola | echo hola2|: &
+#};
+#:
+i=0
+#prueba para usar el kill -19 para parar el proceso
+while true; do
+    echo $i
+    # i=$(( $i+1))
+    let i=$i+1
+    sleep 0.5
+done
+
